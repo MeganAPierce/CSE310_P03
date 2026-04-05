90f6260715dd3a6be9eb8995465127ed453e4cc1
MeganPierce <meganpierce08@gmail.com>
2026-04-04 19:38:49 -0500

    Calling to validate arguments, skeleton of stdin processing

--- main.cpp
+++ main.cpp
@@ -3,13 +3,40 @@
 #include "stack.h"
 #include "main.h"
 #include <iostream>
+#include <cstdlib>
 
-int main(){
+int main(int argc, char* argv[]){
 
     //validate command-line arguments
-    //read the graph file
+    if(!validateArguments(argc, argv)){
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    std::string graphType = argv[2];
+    int flag = std::stoi(argv[3]);
+    
     //create the graph, heap and stack 
+    
     //process stdin instructions
+    std:: string command;
+    while(std::cin >> command){
+        if(command == "Stop"){
+            break;
+        } else if (command == "PrintADJ"){
+
+        } else if (command == "SinglePair"){
+
+        } else if (command == "SingleSource"){
+
+        } else if (command == "PrintPath"){
+
+        } else if (command == "PrintLength"){
+
+        } else {
+            std::cerr << "Invalid instruction.\n";
+        }
+    }
     //clean up memory at the end!!
 
     return 0;
