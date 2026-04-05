02f14b44f247f09fee927885997cfe64e648b6db
MeganPierce <meganpierce08@gmail.com>
2026-04-04 19:29:11 -0500

    Adding input validation

--- util.h
+++ util.h
@@ -1,28 +1,10 @@
-#ifndef _util_h
-#define _util_h 1
-#include <string>
+#ifndef UTIL_H
+#define UTIL_H 
+#include<string>
 
-enum CommandType{
-    CMD_STOP,
-    CMD_READ,
-    CMD_PRINT_ARRAY,
-    CMD_PRINT_HEAP,
-    CMD_BUILD_HEAP,
-    CMD_INSERT,
-    CMD_EXTRACT_MIN,
-    CMD_DECREASE_KEY,
-    CMD_WRITE,
-    CMD_UNKNOWN
-};
-
-struct Command{
-    CommandType type = CMD_UNKNOWN;
-    int index = 0; //for insert and decrease key
-    double newKey = 0.0; //for DecreaseKey
-    std::string raw; //store original command string for error messages  
-};
-
-//parses instructions from stdin, figures out what command user typed
-bool readCommand(Command &cmd);
+bool validateArguments(int argc, char* argv[]);
+bool isValidGraphType(const std::string& graphType);
+bool isValidFlag(const std::string& flagStr);
+bool fileExists(const std::string& filename);
 
 #endif
